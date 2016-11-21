52e4b2f81f9ce4ce81c27600d406282ef10e90a3
NicolasBauth <nicolas.bauthier.01@student.henallux.be>
2016-11-21 12:16:27 +0100

    APIv2

---
+++ JoinUSAPITest/RestAPI/Model/JoinUsContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class JoinUsContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public JoinUsContext()
+            : base(@"Data Source=(localdb)\MSSQLLocalDb;Initial Catalog=RestApiDemo;")
+        {
+
+        }
+    }
+}
