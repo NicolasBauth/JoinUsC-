1d0244bc141c5c1903c9bd1b9ccc59f1b9469d6d
Jennifer Denis <jennifer.brody.denis@gmail.com>
2016-11-23 13:52:09 +0100

    WebAPI v3

---
+++ JoinUSAPITest_verifie/RestAPI/BankApi/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+using System.Data.Entity;
+
+[assembly: OwinStartup(typeof(JoinUsAPI.Startup))]
+
+namespace JoinUsAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Models.ApplicationDbContext>());
+        }
+    }
+}
