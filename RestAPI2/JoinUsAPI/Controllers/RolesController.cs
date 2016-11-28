235904f2031958ac50f07305e12f3c191627ede9
Jennifer Denis <jennifer.brody.denis@gmail.com>
2016-11-28 09:29:20 +0100

    webapi (finie)

---
+++ RestAPI2/JoinUsAPI/Controllers/RolesController.cs
@@ -0,0 +1,209 @@
+﻿using JoinUsAPI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace JoinUsAPI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [RoutePrefix("api/roles")]
+    public class RolesController : ApiController
+    {
+        private ApplicationUserManager _userManager;
+        private ApplicationRoleManager _AppRoleManager;
+        private ModelFactory _modelFactory;
+
+        public ApplicationUserManager AppUserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        protected ApplicationRoleManager AppRoleManager
+        {
+            get
+            {
+                return _AppRoleManager ?? Request.GetOwinContext().GetUserManager<ApplicationRoleManager>();
+            }
+            private set
+            {
+                _AppRoleManager = value;
+            }
+        }
+        protected ModelFactory TheModelFactory
+        {
+            get
+            {
+                if (_modelFactory == null)
+                {
+                    _modelFactory = new ModelFactory(this.Request, this.AppUserManager);
+                }
+                return _modelFactory;
+            }
+        }
+
+        [Route("{id:guid}", Name = "GetRoleById")]
+        public async Task<IHttpActionResult> GetRole(string Id)
+        {
+            var role = await this.AppRoleManager.FindByIdAsync(Id);
+
+            if (role != null)
+            {
+                return Ok(TheModelFactory.Create(role));
+            }
+
+            return NotFound();
+
+        }
+
+        [Route("", Name = "GetAllRoles")]
+        public IHttpActionResult GetAllRoles()
+        {
+            var roles = this.AppRoleManager.Roles;
+
+            return Ok(roles);
+        }
+
+        [Route("create")]
+        public async Task<IHttpActionResult> Create(CreateRoleBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var role = new IdentityRole { Name = model.Name };
+
+            var result = await this.AppRoleManager.CreateAsync(role);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            Uri locationHeader = new Uri(Url.Link("GetRoleById", new { id = role.Id }));
+
+            return Created(locationHeader, TheModelFactory.Create(role));
+
+        }
+
+        [Route("{id:guid}")]
+        public async Task<IHttpActionResult> DeleteRole(string Id)
+        {
+
+            var role = await this.AppRoleManager.FindByIdAsync(Id);
+
+            if (role != null)
+            {
+                IdentityResult result = await this.AppRoleManager.DeleteAsync(role);
+
+                if (!result.Succeeded)
+                {
+                    return GetErrorResult(result);
+                }
+
+                return Ok();
+            }
+
+            return NotFound();
+
+        }
+
+        [Route("ManageUsersInRole")]
+        public async Task<IHttpActionResult> ManageUsersInRole(UsersInRoleModel model)
+        {
+            var role = await this.AppRoleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ModelState.AddModelError("", "Role does not exist");
+                return BadRequest(ModelState);
+            }
+
+            foreach (string user in model.EnrolledUsers)
+            {
+                var appUser = await this.AppUserManager.FindByIdAsync(user);
+
+                if (appUser == null)
+                {
+                    ModelState.AddModelError("", String.Format("User: {0} does not exists", user));
+                    continue;
+                }
+
+                if (!this.AppUserManager.IsInRole(user, role.Name))
+                {
+                    IdentityResult result = await this.AppUserManager.AddToRoleAsync(user, role.Name);
+
+                    if (!result.Succeeded)
+                    {
+                        ModelState.AddModelError("", String.Format("User: {0} could not be added to role", user));
+                    }
+
+                }
+            }
+
+            foreach (string user in model.RemovedUsers)
+            {
+                var appUser = await this.AppUserManager.FindByIdAsync(user);
+
+                if (appUser == null)
+                {
+                    ModelState.AddModelError("", String.Format("User: {0} does not exists", user));
+                    continue;
+                }
+
+                IdentityResult result = await this.AppUserManager.RemoveFromRoleAsync(user, role.Name);
+
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", String.Format("User: {0} could not be removed from role", user));
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RestAPI2/JoinUsAPI/Models/RoleBindingModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace JoinUsAPI.Models
+{
+    public class CreateRoleBindingModel
+    {
+
+        [Required]
+        [StringLength(256, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        [Display(Name = "Role Name")]
+        public string Name { get; set; }
+
+    }
+
+    public class UsersInRoleModel
+    {
+
+        public string Id { get; set; }
+        public List<string> EnrolledUsers { get; set; }
+        public List<string> RemovedUsers { get; set; }
+    }
+
+    public class UserReturnModel
+    {
+        //public string Url { get; set; }
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public IList<string> Roles { get; set; }
+        //public IList<Claim> Claims { get; set; }
+
+    }
+
+    public class RoleReturnModel
+    {
+        //public string Url { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
